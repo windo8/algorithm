09a65cc4cb838e437fdda1c3bc6211941928fd4d
wd8 <jun@zhato.com>
2024-05-31 20:13:56 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+        int even = 0;
+        int odd = 0;
+        for(int i : num_list) {
+            if(i % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        answer[0] = even;
+        answer[1] = odd;
+        
+        return answer;
+    }
+}
