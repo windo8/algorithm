a1d1b48fc2d80be7984874e158869e804b4c8669
wd8 <jun@zhato.com>
2024-05-31 20:14:29 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        answer = my_string.replace(letter, "");
+        return answer;
+    }
+}
